3a57d1e7e145c330415d7e60dde56e4f05c9dbfc
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-02-20 09:28:21 +0100

    Add files via upload

---
+++ dom 1-6/index.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName("li")[2].innerHTML= "Wojtek"
+
+document.querySelector("li a").style.color = "red";
+
+document.querySelector("button").style.backgroundColor = "yellow";
+
+
+document.querySelector("h1").classList.add("huge");
